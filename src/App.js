08267cb54dcfa1497d5d104cef82f6c04068b7f1
Gayaz095 <md.gayazahmed95@gmail.com>
2023-06-09 18:08:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [sorting, setSorting] = useState([]);
+  const [show, setShow] = useState(false);
+
+  //fetchData to fetch data from url
+  const fetchData = async () => {
+    const res = await fetch("https://restcountries.com/v3.1/all");
+    const jsonData = await res.json();
+    setData(jsonData);
+  };
+
+  //handleSort function to sort data in ascending order
+  const handleSort = () => {
+    data.sort((a, b) => {
+      return a.population - b.population;
+    });
+    setSorting(data);
+    //toggling the show
+    setShow((prev) => !prev);
+  };
+
+  useEffect(() => {
+    //using fetchData() when useEffect() executes
+    fetchData();
+    //component re-renders when show is updated
+  }, [show]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handleSort}>Sort Data</button>
+      {/* a toggle show is used in handleSort to show sorted data and previous data */}
+      {show ? (
+        <ol>
+          {sorting &&
+            sorting.map((item) => (
+              <li key={item.flag}>
+                Name:{item.name.common} -- Population: {item.population}
+              </li>
+            ))}
+        </ol>
+      ) : (
+        <ol>
+          {/* only when data is present data is shown and
+            using map() to iterate over data and display data in list form*/}
+          {data &&
+            data.map((item) => (
+              //key is used to avoid error of duplicate children in lists.
+              <li key={item.flag}>
+                Name:{item.name.common} -- Population: {item.population}
+              </li>
+            ))}
+        </ol>
+      )}
+    </>
   );
 }
 
